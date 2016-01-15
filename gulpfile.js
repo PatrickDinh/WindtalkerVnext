83012e804c3d01fcdf68dd5755048b4cf3ac6615
Patrick Dinh <d.conghoang@gmail.com>
2016-01-15 13:12:15 +1000

    Configure angular 2, ts and gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,36 @@
 /// <binding Clean='clean' />
 "use strict";
-var gulp = require("gulp"),
-  rimraf = require("rimraf"),
-  concat = require("gulp-concat"),
-  cssmin = require("gulp-cssmin"),
-  uglify = require("gulp-uglify"),
-  project = require("./project.json");
+var gulp = require("gulp");
+var del = require("del");
+var typeScript = require("gulp-typescript");
+var project = require("./project.json");
+var webroot = "./wwwroot/";
 
-var paths = {
-  webroot: "./wwwroot/"
+var config = {
+    libBase: 'node_modules',
+    lib: [
+        require.resolve('bootstrap/dist/css/bootstrap.css'),
+        require.resolve('systemjs/dist/system.src.js'),
+        require.resolve('rxjs/bundles/Rx.js'),
+        require.resolve('angular2/bundles/angular2.dev.js'),
+        require.resolve('angular2/bundles/angular2-polyfills.js'),
+        require.resolve('angular2/bundles/router.dev.js'),
+        require.resolve('angular2/bundles/http.dev.js'),
+        require.resolve('bootstrap/dist/js/bootstrap.js')
+    ]
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
-
-gulp.task("clean:js", function(cb) {
-  rimraf(paths.concatJsDest, cb);
+gulp.task('build.lib', function () {
+    return gulp.src(config.lib, { base: config.libBase })
+               .pipe(gulp.dest(webroot + 'lib'));
 });
 
-gulp.task("clean:css", function(cb) {
-  rimraf(paths.concatCssDest, cb);
+gulp.task('build', ['build.lib'], function () {
+
 });
 
-gulp.task("clean", ["clean:js", "clean:css"]);
-
-gulp.task("min:js", function() {
-  gulp.src([paths.js, "!" + paths.minJs], {
-      base: "."
-    })
-    .pipe(concat(paths.concatJsDest))
-    .pipe(uglify())
-    .pipe(gulp.dest("."));
+gulp.task('clean', function () {
+    return del([webroot + 'lib']);
 });
 
-gulp.task("min:css", function() {
-  gulp.src([paths.css, "!" + paths.minCss])
-    .pipe(concat(paths.concatCssDest))
-    .pipe(cssmin())
-    .pipe(gulp.dest("."));
-});
-
-gulp.task("min", ["min:js", "min:css"]);
+gulp.task('default', ['build']);
